064b27dc8d87a5ddbd91ae291748e0287c4c39b5
SEUNGHAN BAEK <skud11311@gmail.com>
2023-12-22 23:40:28 +0900

    TodoResponse 생성일시 프로퍼티 추가

--- src/main/kotlin/com/todo/todoapplication/domain/todo/dto/TodoResponse.kt
+++ src/main/kotlin/com/todo/todoapplication/domain/todo/dto/TodoResponse.kt
@@ -1,10 +1,12 @@
 package com.todo.todoapplication.domain.todo.dto
 
 import com.todo.todoapplication.domain.todo.model.Todo
+import java.time.LocalDateTime
 
 data class TodoResponse(
     val title: String,
     val description: String,
+    val creationTime: LocalDateTime,
     val author: String
 ) {
     companion object {
@@ -12,6 +14,7 @@ data class TodoResponse(
             return TodoResponse(
                 title = todo.title,
                 description = todo.description,
+                creationTime = todo.creationTime,
                 author = todo.author
             )
         }
