06286d69441d3e3660dfb46ddc375788a0862b38
SEUNGHAN BAEK <skud11311@gmail.com>
2023-12-22 23:40:21 +0900

    TodoService 1건 불러오기/ 전체 불러오기 비즈니스 로직 작성

--- src/main/kotlin/com/todo/todoapplication/domain/todo/service/TodoService.kt
+++ src/main/kotlin/com/todo/todoapplication/domain/todo/service/TodoService.kt
@@ -3,6 +3,8 @@ package com.todo.todoapplication.domain.todo.service
 import com.todo.todoapplication.domain.todo.dto.TodoCreationRequest
 import com.todo.todoapplication.domain.todo.dto.TodoResponse
 import com.todo.todoapplication.domain.todo.repository.TodoRepository
+import org.springframework.data.domain.Sort
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -14,5 +16,17 @@ class TodoService(private val todoRepository: TodoRepository) {
     fun addTodo(request: TodoCreationRequest): TodoResponse {
         val todo = todoRepository.save(request.toEntity())
         return TodoResponse.from(todo)
+    }
+
+    // R
+    fun getTodo(todoId: Long): TodoResponse {
+        val todo = todoRepository.findByIdOrNull(todoId) ?: throw RuntimeException("$todoId 를 찾을 수 없습니다.}")
+        return TodoResponse.from(todo)
+    }
+
+    fun getTodoList(): List<TodoResponse> {
+        return todoRepository.findAll(Sort.by(Sort.Direction.DESC, "creation_time")).map {
+            TodoResponse.from(it)
+        }
     }
 }
