aefe00d04ab9b120e64dfc04dff29c7d82472c34
SEUNGHAN BAEK <skud11311@gmail.com>
2023-12-22 21:28:46 +0900

    JPA 적용을 위한 allOpen / noArg / jpa 플러그인 추가 및 JPA 의존성 추가 Request DTO Validation을 위해 spring-boot-starter-validation 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,19 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
+    kotlin("plugin.jpa") version "1.8.22"
+}
+
+noArg {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
 }
 
 group = "com.todo"
@@ -22,6 +35,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
