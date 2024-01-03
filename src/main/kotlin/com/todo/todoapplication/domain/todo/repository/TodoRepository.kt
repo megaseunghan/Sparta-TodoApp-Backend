4ae11403eb0b67147a8f717fde1a1885db12a59f
SEUNGHAN BAEK <skud11311@gmail.com>
2024-01-03 15:21:57 +0900

    TodoRepository @Repository 어노테이션 추가

--- src/main/kotlin/com/todo/todoapplication/domain/todo/repository/TodoRepository.kt
+++ src/main/kotlin/com/todo/todoapplication/domain/todo/repository/TodoRepository.kt
@@ -2,6 +2,8 @@ package com.todo.todoapplication.domain.todo.repository
 
 import com.todo.todoapplication.domain.todo.model.Todo
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface TodoRepository : JpaRepository<Todo, Long> {
 }
